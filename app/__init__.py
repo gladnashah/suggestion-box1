e0e54f35ecc929fddbc825c8b07fcf315c948e89
Gladys <gladysmumbua@gmail.com>
2016-05-04 07:59:41 +0300

    app __init__,manage.py  modified

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,24 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-def create_app(config_name):
-    login_manager.init_app(app)
-# lm = LoginManager()
-# lm.init_app(app)
-# oid = OpenID(app, os.path.join(basedir, 'tmp'))
+def create_app(config):
+	app = Flask(__name__)
+	app.config.from_object(config[config])
+	config[config].init_app(app)
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+	db = SQLAlchemy(app)
+	db.init_app(app)
+	bootstrap.init_app(app)
+	moment.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+	toolbar = DebugToolbarExtension(app)
 
-from app import views, models
+	
 
+	from app import  models
+	from . import views
+
+	return app
+
+	
